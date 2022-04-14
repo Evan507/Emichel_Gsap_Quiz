66bc6eb257d31aa62fc91f003f3e16a890c171f3
Evan <81766815+Evan507@users.noreply.github.com>
2022-04-14 19:47:29 -0400

    part 1 finished

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,7 +1,18 @@
 //import { gsap } from "gsap";
 
 //Replace this value with your name
-const yourName = "Your Name"
+const yourName = "Evan Michel"
 document.querySelector("#name").innerHTML = yourName;
 
 //GreenSock Timeline
+
+import { gsap } from "gsap";
+
+const quiz = gsap.timeline();
+    
+quiz.from(".red", {duration:1, x:0}, "horiz")
+.from(".blue", {duration:1, x:350}, "horiz")
+.to(".red" ,{duration:1, y:300}, "vert")
+.to(".blue", {duration:1, y:0}, "vert")
+
+;
