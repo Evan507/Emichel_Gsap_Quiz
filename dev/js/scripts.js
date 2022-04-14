801fcb47f59d60c9bad49eaa6a0b30b7fa442a4b
Evan <81766815+Evan507@users.noreply.github.com>
2022-04-14 19:57:11 -0400

    step 2 complete

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -10,9 +10,12 @@ import { gsap } from "gsap";
 
 const quiz = gsap.timeline();
     
-quiz.from(".red", {duration:1, x:0}, "horiz")
+quiz
+.from(".graph", {duration: 2, opacity:0})
+.from(".red", {duration:1, x:0}, "horiz")
 .from(".blue", {duration:1, x:350}, "horiz")
-.to(".red" ,{duration:1, y:300}, "vert")
-.to(".blue", {duration:1, y:0}, "vert")
+.to(".red" ,{duration:2, y:300}, "vert")
+.to(".blue", {duration:2, y:0}, "vert")
+.from(".green", {duration: 3, width:50}, "horiz","vert")
 
 ;
