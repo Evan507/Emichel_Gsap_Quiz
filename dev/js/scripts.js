130f75bb175b268ecf5f13facb77b55bdff56994
Evan <81766815+Evan507@users.noreply.github.com>
2022-04-14 20:06:12 -0400

    step 3 mostly complete

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -12,10 +12,12 @@ const quiz = gsap.timeline();
     
 quiz
 .from(".graph", {duration: 2, opacity:0})
-.from(".red", {duration:1, x:0}, "horiz")
-.from(".blue", {duration:1, x:350}, "horiz")
+.from(".red", {duration:1, x:0, ease: "bounce.out"}, "horiz")
+.from(".blue", {duration:1, x:350, ease: "bounce.out"}, "horiz")
 .to(".red" ,{duration:2, y:300}, "vert")
 .to(".blue", {duration:2, y:0}, "vert")
 .from(".green", {duration: 3, width:50}, "horiz","vert")
+.to(".red" ,{duration:0.5, delay:1, rotation:360, ease: "elastic.out"}, "vert")
+.to(".blue", {duration:0.5, delay:1, rotation:360, ease: "elastic.out"}, "vert")
 
 ;
